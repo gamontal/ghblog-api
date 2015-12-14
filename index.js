d8e0ed8cce8ab38081efaab7fca142049bac6fe3
Gabriel Montalvo <gabe.mon5@outlook.com>
2015-12-14 18:56:59 -0400

    update
    can’t seem to make this work yet.

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ var pagesLib = require('./lib/pages');
 // until it gets a null response and sends an object back with all the pages of a given blog section
 module.exports = function(category) {
     return new Promise(function(resolve, reject) {
-       var pages = [];
-       for (var pnum = 1; pnum <= 9; pnum++) {
+       var pages = [], pnum = 1;
+       //for (var pnum = 1; pnum <= 9; pnum++) {
         pagesLib(category, pnum).then(function(data) {
-            if(data === null) { return resolve(pages); } else {
-                pages = data;
-            }
+            //if(data === null) { return resolve(pages); } else {
+                return resolve(data);
+            //}
         });
-       }
+       //}
     });
 };
-// LAST_UPDATED: 12/13/15 - 9:20 pm
+// LAST_UPDATED: 12/14/15 - 6:56 pm
